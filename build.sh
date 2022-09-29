152f16c37814affd5a0de10918c294ff26544532
Denis <77270222+ayonovdenizs@users.noreply.github.com>
2022-09-29 12:29:33 +0600

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,6 @@
+sudo apt install python
+sudo apt install pip
+pip install --upgrade pip
+pip install -r install.txt
+pyinstaller -r -W PCToolsBot.py
+move dist/PCToolsBot.exe Build.py.PCToolsBot.exe
