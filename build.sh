d116d00f0b7fe6fe70591687526ca0cf35e4c06f
Denis <77270222+ayonovdenizs@users.noreply.github.com>
2022-09-29 13:05:43 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ sudo apt install python
 sudo apt install pip
 pip install --upgrade pip
 pip install -r install.txt
-pyinstaller -r -W PCToolsBot.py
+pyinstaller -w -F PCToolsBot.py
 mv dist/PCToolsBot.exe Build.py.PCToolsBot.exe
